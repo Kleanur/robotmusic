a823fcbb4ff037c4104496c674ef222258c113b8
Kevin Li <34748712+Kleanur@users.noreply.github.com>
2019-11-09 18:24:06 -0500

    yeeted

--- public/robotmusic.py
+++ public/robotmusic.py
@@ -5,12 +5,12 @@ from pyknon.genmidi import Midi
 from pyknon.music import Note
 from pyknon.music import NoteSeq
 
-SOP_RANGE = range(4, 8)
+SOP_RANGE = range(6, 8)
 ALT_RANGE = range(3, 7)
 TEN_RANGE = range(2, 6)
 BAS_RANGE = range(1, 5)
 
-SONG_DURATION = 17
+SONG_DURATION = 16
 RANDOM_PITCHLIST = range(12)
 SCALE_PITCHLIST = [0, 2, 4, 5, 7, 9, 11]
 
@@ -49,22 +49,22 @@ def writesong():
             tenor += random_notes(I_PITCHLIST_AT, TEN_RANGE, 1, 90)
             bass += random_notes(I_PITCHLIST_B, BAS_RANGE, 1, 120)
         elif x % 4 == 0:
-            soprano += random_notes(I_PITCHLIST_AT, SOP_RANGE, 0.125, 120)
+            soprano += random_notes(I_PITCHLIST_AT, SOP_RANGE, 0.0625, 120)
             alto += random_notes(I_PITCHLIST_AT, ALT_RANGE, 0.125, 90)
             tenor += random_notes(I_PITCHLIST_AT, TEN_RANGE, 0.25, 90)
             bass += random_notes(I_PITCHLIST_B, BAS_RANGE, 0.5, 90)
         elif x % 4 == 1:
-            soprano += random_notes(SCALE_PITCHLIST, SOP_RANGE, 0.125, 120)
+            soprano += random_notes(SCALE_PITCHLIST, SOP_RANGE, 0.0625, 120)
             alto += random_notes(IV_PITCHLIST_AT, ALT_RANGE, 0.125, 90)
             tenor += random_notes(IV_PITCHLIST_AT, TEN_RANGE, 0.25, 90)
             bass += random_notes(IV_PITCHLIST_B, BAS_RANGE, 0.5, 90)
         elif x % 4 == 2:
-            soprano += random_notes(SCALE_PITCHLIST, SOP_RANGE, 0.125, 120)
+            soprano += random_notes(SCALE_PITCHLIST, SOP_RANGE, 0.0625, 120)
             alto += random_notes(V_PITCHLIST_AT, ALT_RANGE, 0.125, 90)
             tenor += random_notes(V_PITCHLIST_AT, TEN_RANGE, 0.25, 90)
             bass += random_notes(V_PITCHLIST_B, BAS_RANGE, 0.5, 90)
         elif x % 4 == 3:
-            soprano += random_notes(SCALE_PITCHLIST, SOP_RANGE, 0.125, 120)
+            soprano += random_notes(SCALE_PITCHLIST, SOP_RANGE, 0.0625, 120)
             alto += random_notes(VI_PITCHLIST_AT, ALT_RANGE, 0.125, 90)
             tenor += random_notes(VI_PITCHLIST_AT, TEN_RANGE, 0.25, 90)
             bass += random_notes(VI_PITCHLIST_B, BAS_RANGE, 0.5, 90)
